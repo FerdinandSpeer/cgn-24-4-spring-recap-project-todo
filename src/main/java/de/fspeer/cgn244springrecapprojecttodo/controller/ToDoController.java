7151b7520e7bc3ee548372770384af34dc8d13fa
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-16 16:16:37 +0200

    First CRUD methods

--- src/main/java/de/fspeer/cgn244springrecapprojecttodo/controller/ToDoController.java
+++ src/main/java/de/fspeer/cgn244springrecapprojecttodo/controller/ToDoController.java
@@ -1,11 +1,14 @@
 package de.fspeer.cgn244springrecapprojecttodo.controller;
 
 
+import de.fspeer.cgn244springrecapprojecttodo.models.ToDo;
+import de.fspeer.cgn244springrecapprojecttodo.models.ToDoDTO;
+import de.fspeer.cgn244springrecapprojecttodo.repository.ToDoRepository;
 import de.fspeer.cgn244springrecapprojecttodo.service.ToDoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
@@ -15,7 +18,12 @@ public class ToDoController {
     public final ToDoService toDoService;
 
     @GetMapping
-    public
+    public List<ToDo> getAllToDos(){
+        return toDoService.getAllToDos();
+    }
 
-
+    @PostMapping
+    public ToDo createToDo(@RequestBody ToDoDTO toDoDTO){
+        return toDoService.createToDo(toDoDTO);
+    }
 }
--- src/main/java/de/fspeer/cgn244springrecapprojecttodo/service/ToDoService.java
+++ src/main/java/de/fspeer/cgn244springrecapprojecttodo/service/ToDoService.java
@@ -1,10 +1,29 @@
 package de.fspeer.cgn244springrecapprojecttodo.service;
 
+import de.fspeer.cgn244springrecapprojecttodo.models.ToDo;
+import de.fspeer.cgn244springrecapprojecttodo.models.ToDoDTO;
+import de.fspeer.cgn244springrecapprojecttodo.repository.ToDoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class ToDoService {
 
+    private final IdService idService;
+    private final ToDoRepository toDoRepository;
+
+    public List<ToDo> getAllToDos() {
+        return toDoRepository.findAll();
+    }
+
+    public ToDo createToDo(ToDoDTO toDoDTO) {
+        return toDoRepository.save(new ToDo(idService.generateId(), toDoDTO.description(), toDoDTO.status()));
+    }
 }
